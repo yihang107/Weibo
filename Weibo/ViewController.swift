50db63c841cf71d4494979178e2305192ad8bee2
GodDog <yangyihang5@163.com>
2022-04-21 16:37:46 +0800

    Initial Commit

---
+++ Weibo/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Weibo
+//
+//  Created by 狗身狗面狗 on 2022/4/21.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
