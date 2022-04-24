5f22a4e34bb9c751a82ee79bd4fcd1791d707d61
GodDog <yangyihang5@163.com>
2022-04-24 13:12:17 +0800

    accessToken

--- Weibo/Tools/YYHNetworkTools.swift
+++ Weibo/Tools/YYHNetworkTools.swift
@@ -16,16 +16,19 @@ case POST = "POST"
 
 class YYHNetworkTools: AFHTTPSessionManager {
     // MARK: 应用程序消息
-    private let appKey = ""
+    private let appKey = "2045436852"
     private let appSecret = ""
-    private let redirectUrl = ""
+    private let redirectUrl = "https://www.sina.com"
     
+    /// 类似于OC typeDefine 网络请求回调
+    typealias YYHRequestCllBack = (_ result : Any?, _ error: Error?) -> ()
     
     // 单例
     static let sharedTools: YYHNetworkTools = {
         let tools = YYHNetworkTools(baseURL: nil)
         // 设置反序列化数据格式 系统自动将OC中的NSSet转为Set
         tools.responseSerializer.acceptableContentTypes?.insert("text/html")
+        tools.responseSerializer.acceptableContentTypes?.insert("text/plain")
         return tools
     }()
 }
@@ -38,12 +41,23 @@ extension YYHNetworkTools {
         let urlString = "https://api.weibo.com/oauth2/authorize?client_id=\(appKey)&redirect_uri=\(redirectUrl)"
         return URL(string: urlString)!
     }
+    
+    /// 加载AccessToken
+    func loadAccessToken(code: String, finished: @escaping YYHRequestCllBack) {
+        let urlString = "https://api.weibo.com/oauth2/access_token"
+        let params = ["client_id": appKey,
+                      "client_secret": appSecret,
+                      "grant_type": "authorization_code",
+                      "code": code,
+                      "redirect_uri": redirectUrl]
+        request(method: NetworkRequestMethod.POST, URLString: urlString, parameters: params, finished: finished)
+    }
 }
 
 // MARK: 封装AFN网络方法
 extension YYHNetworkTools {
     /// 网络请求
-    func request(method: NetworkRequestMethod, URLString: String, parameters: [String: AnyObject]? , finished:  @escaping(_ result: Any?, _ error: Error?)->()) {
+    private func request(method: NetworkRequestMethod, URLString: String, parameters: [String: String]? , finished:  @escaping YYHRequestCllBack) {
         dataTask(withHTTPMethod: method.rawValue, urlString: URLString, parameters: parameters, headers: nil, uploadProgress: nil, downloadProgress: { _ in
             
         }, success: { _, result in
--- Weibo/View/OAuth/OAuthViewController.swift
+++ Weibo/View/OAuth/OAuthViewController.swift
@@ -19,13 +19,14 @@ class OAuthViewController: UIViewController {
     
     /// 自动填充用户名和密码 以代码的方式向web页面添加内容
     @objc private func autoFill() {
-        let js = "document.getElementById('userId').value = '';" + "document.getElementById('passwd').value = 'qqq123'";
+        let js = "document.getElementById('loginName').value = '17702348271';" + "document.getElementById('loginPassword').value = '111111yy'";
         // 让webView执行js
         webView.evaluateJavaScript(js, completionHandler: nil)
     }
     
     override func loadView() {
         view = webView
+        webView.navigationDelegate = self
         title = "登录新浪微博"
         navigationItem.leftBarButtonItem = UIBarButtonItem(title: "关闭", style: UIBarButtonItem.Style.plain, target: self, action: #selector(self.close))
         navigationItem.rightBarButtonItem = UIBarButtonItem(title: "自动填充", style: UIBarButtonItem.Style.plain, target: self, action: #selector(self.autoFill))
@@ -37,5 +38,42 @@ class OAuthViewController: UIViewController {
         
         // 加载页面
         self.webView.load(URLRequest(url: YYHNetworkTools.sharedTools.oAuthURL))
+    }
+}
+
+
+//MARK: WKWebviewDelegate
+
+
+extension OAuthViewController: WKNavigationDelegate {
+    /// 收到响应之后 是否允许加载
+    func webView(_ webView: WKWebView, decidePolicyFor navigationResponse: WKNavigationResponse, decisionHandler: @escaping (WKNavigationResponsePolicy) -> Void) {
+        guard let url = navigationResponse.response.url, url.host == "www.sina.com" else {
+            decisionHandler(WKNavigationResponsePolicy.allow)
+            return
+        }
+        
+        // 从重定向地址提取code是否存在
+        guard let query = url.query, query.hasPrefix("code=") else {
+            decisionHandler(WKNavigationResponsePolicy.cancel)
+            return
+        }
+        
+        
+        // 提取code=后面的授权码
+        print(url.host!)
+        print(url.query!)
+        let code = query.substring(from: "code=".endIndex)
+        print("授权码是:" + code)
+        
+        // 加载accessToken
+        YYHNetworkTools.sharedTools.loadAccessToken(code: code) { result, error in
+            if error != nil {
+                print("\(String(describing: error))")
+            }
+                      
+            print(result ?? "没有结果")
+        }
+        decisionHandler(WKNavigationResponsePolicy.allow)
     }
 }
